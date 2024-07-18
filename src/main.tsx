c9b550e7f9531c0dac355d59a0b5d7c704c8a093
Juan Cruz Estevez <72108526+juancruzestevez@users.noreply.github.com>
2024-07-18 16:23:52 -0300

    mas requests

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import {Auth0Provider} from "@auth0/auth0-react";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
@@ -16,7 +15,6 @@ createRoot(document.getElementById('root')!).render(
             domain={domain}
             clientId={clientId}
             authorizationParams={{
-                audience: audience,
                 redirect_uri: window.location.origin
             }}>
             <PaginationProvider>
