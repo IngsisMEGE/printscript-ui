9a7b9f714b528fca450d10348c94fc926285bc62
GiorgioJuan <juanmartin.giorgio@ing.austral.edu.ar>
2024-07-19 12:29:37 -0300

    testeito5

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import {PaginationProvider} from "./contexts/paginationProvider.tsx";
 import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
 import {Auth0Provider} from "@auth0/auth0-react";
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Auth0Provider
-            domain="https://dev-0et3cewhpj5hltwn.us.auth0.com/"
-            clientId="4Nri47eYWGw91OMYTzkd1uZCqJwn6oGW"
+            domain={domain}
+            clientId={clientId}
             authorizationParams={{
                 redirect_uri: window.location.origin
             }}>
